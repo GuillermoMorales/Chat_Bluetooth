0e96be5a65b765ab7dc67531d332a7a95b6e1afd
GuillermoMorales <00011417@uca.edu.sv>
2019-06-18 15:19:07 -0600

    Adding permissions & Checking if Bluetooth is enabled

--- app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
+++ app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.javi.chat_bluetooth
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    override fun onStart() {
+
+        //Checking if BLUETOOTH is enabled when we launch the activity
+        super.onStart()
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(this,"El Bluetooth no está disponible",Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
     }
 }
