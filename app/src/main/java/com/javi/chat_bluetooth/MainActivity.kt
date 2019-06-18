9ce4ee68682bdf1fc92b5b329a92ad6cf5f3029a
Donali <00084417@uca.edu.sv>
2019-06-18 17:26:24 -0600

    Added vars to main activity

--- app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
+++ app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.javi.chat_bluetooth
 
+import android.app.Dialog
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.*
+
 
 class MainActivity : AppCompatActivity() {
     val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    lateinit var status: TextView
+    lateinit var btnConnect: Button
+    lateinit var listView: ListView
+    lateinit var dialog: Dialog
+    lateinit var inputLayout: EditText
+    lateinit var chatAdapter: ArrayAdapter<String>
+    lateinit var chatMessages: ArrayList<String>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        if(bluetoothAdapter.isDiscovering())
-        {
-            bluetoothAdapter.cancelDiscovery()
-        }
-        bluetoothAdapter.startDiscovery()
+    val MESSAGE_STATE_CHANGE = 1
+    val MESSAGE_READ = 2
+    val MESSAGE_WRITE = 3
+    val MESSAGE_DEVICE_OBJECT = 4
+    val MESSAGE_TOAST = 5
+    val DEVICE_OBJECT = "device_name"
 
-        //Registro para
-
-    }
 
     override fun onStart() {
 
@@ -32,10 +36,25 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"El Bluetooth no está disponible",Toast.LENGTH_SHORT).show()
             finish()
         }
-        if(!bluetoothAdapter.isEnabled())
+        if(!bluetoothAdapter.isEnabled)
         {
             val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableIntent, 1)
         }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(bluetoothAdapter.isDiscovering)
+        {
+            bluetoothAdapter.cancelDiscovery()
+        }
+        bluetoothAdapter.startDiscovery()
+
+
+    }
+
+
 }
