34b319311cca29dae9f5b66fd7edfe9442501c76
Donali <00084417@uca.edu.sv>
2019-06-18 19:17:47 -0600

    Fixed classes and main activity

--- app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
+++ app/src/main/java/com/javi/chat_bluetooth/MainActivity.kt
@@ -6,57 +6,270 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import android.widget.ArrayAdapter
+import android.bluetooth.BluetoothDevice
+import android.view.View
+import android.widget.Toast
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+import android.widget.AdapterView
+import android.content.IntentFilter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.media.session.PlaybackState.STATE_NONE
+import android.os.Handler
+import android.app.Activity
+import com.javi.chat_bluetooth.UtilClass.Companion.DEVICE_OBJECT
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_DEVICE_OBJECT
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_READ
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_STATE_CHANGE
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_TOAST
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_CONNECTED
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_CONNECTING
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_LISTEN
 
 
 class MainActivity : AppCompatActivity() {
-    val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    var bluetoothAdapter: BluetoothAdapter? = null
     lateinit var status: TextView
     lateinit var btnConnect: Button
     lateinit var listView: ListView
     lateinit var dialog: Dialog
-    lateinit var inputLayout: EditText
+    lateinit var inputLayout: TextInputLayout
     lateinit var chatAdapter: ArrayAdapter<String>
     lateinit var chatMessages: ArrayList<String>
 
-    val MESSAGE_STATE_CHANGE = 1
-    val MESSAGE_READ = 2
-    val MESSAGE_WRITE = 3
-    val MESSAGE_DEVICE_OBJECT = 4
-    val MESSAGE_TOAST = 5
-    val DEVICE_OBJECT = "device_name"
 
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+    lateinit var connectingDevice: BluetoothDevice
+    lateinit var discoveredDevicesAdapter: ArrayAdapter<String>
+    lateinit var chatController: OnlineChat
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewsByIds()
+
+        //check device support bluetooth or not
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "El Bluetooth no está disponible", Toast.LENGTH_SHORT).show();
+            finish()
+        }
+        //show bluetooth devices dialog when click connect button
+        btnConnect.setOnClickListener { showPrinterPickDialog() }
+
+        //set chat adapter
+        chatMessages = ArrayList()
+        chatAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, chatMessages)
+        listView.adapter = chatAdapter
+    }
+
+    private val handler = Handler(Handler.Callback { msg ->
+        when (msg.what) {
+            MESSAGE_STATE_CHANGE -> when (msg.arg1) {
+                STATE_CONNECTED -> {
+                    setStatus("Connected to: " + connectingDevice.name)
+                    btnConnect.isEnabled = false
+                }
+                STATE_CONNECTING -> {
+                    setStatus("Connecting...")
+                    btnConnect.isEnabled = false
+                }
+                STATE_LISTEN, STATE_NONE -> setStatus("Not connected")
+            }
+            UtilClass.MESSAGE_WRITE -> {
+                val writeBuf = msg.obj as ByteArray
+
+                val writeMessage = String(writeBuf)
+                chatMessages.add("Me: $writeMessage")
+                chatAdapter.notifyDataSetChanged()
+            }
+            MESSAGE_READ -> {
+                val readBuf = msg.obj as ByteArray
+
+                val readMessage = String(readBuf, 0, msg.arg1)
+                chatMessages.add(connectingDevice.name + ":  " + readMessage)
+                chatAdapter.notifyDataSetChanged()
+            }
+            MESSAGE_DEVICE_OBJECT -> {
+                connectingDevice = msg.data.getParcelable(DEVICE_OBJECT)!!
+                Toast.makeText(applicationContext, "Connected to " + connectingDevice.name,
+                        Toast.LENGTH_SHORT).show()
+            }
+            MESSAGE_TOAST -> Toast.makeText(applicationContext, msg.getData().getString("toast"),
+                    Toast.LENGTH_SHORT).show()
+        }
+        false
+    })
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_ENABLE_BLUETOOTH -> if (resultCode == Activity.RESULT_OK) {
+                chatController = OnlineChat(this, handler)
+            } else {
+                Toast.makeText(this, "Bluetooth still disabled, turn off application!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+
+        if (chatController.getState() == STATE_NONE) {
+            chatController.start()
+        }
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        chatController.stop()
+    }
 
     override fun onStart() {
 
         //Checking if BLUETOOTH is enabled when we launch the activity
         super.onStart()
-        if(bluetoothAdapter == null)
-        {
-            Toast.makeText(this,"El Bluetooth no está disponible",Toast.LENGTH_SHORT).show()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "El Bluetooth no está disponible", Toast.LENGTH_SHORT).show()
             finish()
         }
 
 
-        if (!bluetoothAdapter.isEnabled) {
+        if (!bluetoothAdapter!!.isEnabled) {
             val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            //startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH)
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH)
         } else {
-            //onlineChat = OnlineChat(this, handler)
+            chatController = OnlineChat(this, handler)
+
+
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        if(bluetoothAdapter.isDiscovering)
-        {
-            bluetoothAdapter.cancelDiscovery()
-        }
-        bluetoothAdapter.startDiscovery()
-
-
+    private fun setStatus(s: String) {
+        status.text = s
     }
+
+    private fun findViewsByIds() {
+        status = findViewById(R.id.status)
+        btnConnect = findViewById(R.id.btn_connect)
+        listView = findViewById(R.id.list)
+        inputLayout = findViewById(R.id.input_layout)
+        val btnSend = findViewById(R.id.btn_send)
+
+        btnSend.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                if (inputLayout.editText?.text.toString() == "") {
+                    Toast.makeText(this@MainActivity, "Please input some texts", Toast.LENGTH_SHORT).show()
+                } else {
+                    //TODO: here
+                    sendMessage(inputLayout.editText?.text.toString())
+                    inputLayout.editText?.setText("")
+                }
+            }
+        })
+    }
+
+    private fun showPrinterPickDialog() {
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.layout_bluetooth)
+        dialog.setTitle("Bluetooth Devices")
+
+        if (bluetoothAdapter!!.isDiscovering) {
+            bluetoothAdapter!!.cancelDiscovery()
+        }
+        bluetoothAdapter!!.startDiscovery()
+
+        //Initializing bluetooth adapters
+        val pairedDevicesAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        discoveredDevicesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+
+        //locate listviews and attatch the adapters
+        val listView = dialog.findViewById(R.id.pairedDeviceList) as ListView
+        val listView2 = dialog.findViewById(R.id.discoveredDeviceList) as ListView
+        listView.adapter = pairedDevicesAdapter
+        listView2.adapter = discoveredDevicesAdapter
+
+        // Register for broadcasts when a device is discovered
+        var filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(discoveryFinishReceiver, filter)
+
+        // Register for broadcasts when discovery has finished
+        filter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        registerReceiver(discoveryFinishReceiver, filter)
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = bluetoothAdapter!!.bondedDevices
+
+        // If there are paired devices, add each one to the ArrayAdapter
+        if (pairedDevices.size > 0) {
+            for (device in pairedDevices) {
+                pairedDevicesAdapter.add(device.name + "\n" + device.address)
+            }
+        } else {
+            pairedDevicesAdapter.add(getString(R.string.none_paired))
+        }
+
+        //Handling listview item click event
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, view, _, _ ->
+            bluetoothAdapter!!.cancelDiscovery()
+            val info = (view as TextView).text.toString()
+            val address = info.substring(info.length - 17)
+
+            connectToDevice(address)
+            dialog.dismiss()
+        }
+
+        listView2.onItemClickListener = AdapterView.OnItemClickListener { _, view, _, _ ->
+            bluetoothAdapter!!.cancelDiscovery()
+            val info = (view as TextView).text.toString()
+            val address = info.substring(info.length - 17)
+
+            connectToDevice(address)
+            dialog.dismiss()
+        }
+
+        dialog.findViewById(R.id.cancelButton).setOnClickListener(View.OnClickListener { dialog.dismiss() })
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+
+    private val discoveryFinishReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                if (device.bondState != BluetoothDevice.BOND_BONDED) {
+                    discoveredDevicesAdapter.add(device.name + "\n" + device.address)
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
+                if (discoveredDevicesAdapter.count == 0) {
+                    discoveredDevicesAdapter.add(getString(R.string.none_found))
+                }
+            }
+        }
+    }
+
+    private fun connectToDevice(deviceAddress: String) {
+        bluetoothAdapter!!.cancelDiscovery()
+        val device = bluetoothAdapter!!.getRemoteDevice(deviceAddress)
+        chatController.connect(device)
+    }
+
+    private fun sendMessage(message: String) {
+        if (chatController.getState() != STATE_CONNECTED) {
+            Toast.makeText(this, "Connection was lost!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (message.isNotEmpty()) {
+            val send = message.toByteArray()
+            chatController.write(send)
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/javi/chat_bluetooth/OnlineChat.kt
+++ app/src/main/java/com/javi/chat_bluetooth/OnlineChat.kt
@@ -7,107 +7,95 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
+import com.javi.chat_bluetooth.UtilClass.Companion.DEVICE_OBJECT
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_DEVICE_OBJECT
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_READ
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_STATE_CHANGE
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_TOAST
+import com.javi.chat_bluetooth.UtilClass.Companion.MESSAGE_WRITE
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_CONNECTED
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_CONNECTING
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_LISTEN
+import com.javi.chat_bluetooth.UtilClass.Companion.STATE_NONE
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
-import android.media.session.PlaybackState.STATE_NONE
-import android.R.attr.start
-import android.R.string.cancel
-import android.R.attr.start
-import android.R.string.cancel
-import android.R.attr.start
-import android.R.string.cancel
-import android.media.session.PlaybackState.STATE_NONE
-import android.R.string.cancel
-import android.R.attr.start
-import android.R.attr.start
-import android.media.session.PlaybackState.STATE_NONE
-import android.R.attr.start
 
-class OnlineChat {
 
-    init {
+class OnlineChat(context: Context, handler: Handler) {
 
-    }
 
     private val APP_NAME = "BluetoothChatApp"
     private val MY_UUID = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66")
 
     private var bluetoothAdapter: BluetoothAdapter? = null
-    private val handler: Handler? = null
+    private var handler: Handler? = null
     private var acceptThread: AcceptThread? = null
     private var connectThread: ConnectThread? = null
     private var connectedThread: ReadWriteThread? = null
-    private var state: Int = 0
-
-    val STATE_NONE = 0
-    val STATE_LISTEN = 1
-    val STATE_CONNECTING = 2
-    val STATE_CONNECTED = 3
-    val STATE_OFF = 4
+    private var currentState: Int = 0
 
 
-    /*fun OnlineChat(context: Context, handler: Handler): ??? {
+    init {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        state = STATE_NONE
-
+        currentState = STATE_NONE
         this.handler = handler
-    }*/
+    }
 
     @Synchronized
     private fun setState(state: Int) {
-        this.state = state
+        this.currentState = state
 
-        handler.obtainMessage(MainActivity.MESSAGE_STATE_CHANGE, state, -1).sendToTarget()
+        handler!!.obtainMessage(MESSAGE_STATE_CHANGE, state, -1).sendToTarget()
     }
 
     @Synchronized
     fun getState(): Int {
-        return state
+        return currentState
     }
 
     @Synchronized
     fun start() {
         // Cancel any thread
         if (connectThread != null) {
-            connectThread.cancel()
+            connectThread!!.cancel()
             connectThread = null
         }
 
         // Cancel any running thresd
         if (connectedThread != null) {
-            connectedThread.cancel()
+            connectedThread!!.cancel()
             connectedThread = null
         }
 
         setState(STATE_LISTEN)
         if (acceptThread == null) {
             acceptThread = AcceptThread()
-            acceptThread.start()
+            acceptThread!!.start()
         }
     }
+
     @Synchronized
     fun connect(device: BluetoothDevice) {
         // Cancel any thread
-        if (state === STATE_CONNECTING) {
+        if (currentState == STATE_CONNECTING) {
             if (connectThread != null) {
-                connectThread.cancel()
+                connectThread!!.cancel()
                 connectThread = null
             }
         }
 
         // Cancel running thread
         if (connectedThread != null) {
-            connectedThread.cancel()
+            connectedThread!!.cancel()
             connectedThread = null
         }
 
         // Start the thread to connect with the given device
         connectThread = ConnectThread(device)
-        connectThread.start()
+        connectThread!!.start()
         setState(STATE_CONNECTING)
     }
 
@@ -116,31 +104,31 @@ class OnlineChat {
     fun connected(socket: BluetoothSocket, device: BluetoothDevice) {
         // Cancel the thread
         if (connectThread != null) {
-            connectThread.cancel()
+            connectThread!!.cancel()
             connectThread = null
         }
 
         // Cancel running thread
         if (connectedThread != null) {
-            connectedThread.cancel()
+            connectedThread!!.cancel()
             connectedThread = null
         }
 
         if (acceptThread != null) {
-            acceptThread.cancel()
+            acceptThread!!.cancel()
             acceptThread = null
         }
 
         // Start the thread to manage the connection and perform transmissions
         connectedThread = ReadWriteThread(socket)
-        connectedThread.start()
+        connectedThread!!.start()
 
         // Send the name of the connected device back to the UI Activity
-        val msg = handler.obtainMessage(MainActivity.MESSAGE_DEVICE_OBJECT)
+        val msg = handler!!.obtainMessage(MESSAGE_DEVICE_OBJECT)
         val bundle = Bundle()
-        bundle.putParcelable(MainActivity.DEVICE_OBJECT, device)
+        bundle.putParcelable(DEVICE_OBJECT, device)
         msg.data = bundle
-        handler.sendMessage(msg)
+        handler!!.sendMessage(msg)
 
         setState(STATE_CONNECTED)
     }
@@ -148,17 +136,17 @@ class OnlineChat {
     @Synchronized
     fun stop() {
         if (connectThread != null) {
-            connectThread.cancel()
+            connectThread!!.cancel()
             connectThread = null
         }
 
         if (connectedThread != null) {
-            connectedThread.cancel()
+            connectedThread!!.cancel()
             connectedThread = null
         }
 
         if (acceptThread != null) {
-            acceptThread.cancel()
+            acceptThread!!.cancel()
             acceptThread = null
         }
         setState(STATE_NONE)
@@ -167,30 +155,30 @@ class OnlineChat {
     fun write(out: ByteArray) {
         val r: ReadWriteThread
         synchronized(this) {
-            if (state !== STATE_CONNECTED)
+            if (currentState != STATE_CONNECTED)
                 return
-            r = connectedThread
+            r = connectedThread!!
         }
         r.write(out)
     }
 
     private fun connectionFailed() {
-        val msg = handler.obtainMessage(MainActivity.MESSAGE_TOAST)
+        val msg = handler!!.obtainMessage(MESSAGE_TOAST)
         val bundle = Bundle()
         bundle.putString("toast", "Unable to connect device")
         msg.data = bundle
-        handler.sendMessage(msg)
+        handler!!.sendMessage(msg)
 
         // Start the service over to restart listening mode
         this@OnlineChat.start()
     }
 
     private fun connectionLost() {
-        val msg = handler.obtainMessage(MainActivity.MESSAGE_TOAST)
+        val msg = handler!!.obtainMessage(MESSAGE_TOAST)
         val bundle = Bundle()
         bundle.putString("toast", "Device connection was lost")
         msg.data = bundle
-        handler.sendMessage(msg)
+        handler!!.sendMessage(msg)
 
         // Start the service over to restart listening mode
         this@OnlineChat.start()
@@ -202,7 +190,7 @@ class OnlineChat {
         init {
             var tmp: BluetoothServerSocket? = null
             try {
-                tmp = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(APP_NAME, MY_UUID)
+                tmp = bluetoothAdapter!!.listenUsingInsecureRfcommWithServiceRecord(APP_NAME, MY_UUID)
             } catch (ex: IOException) {
                 ex.printStackTrace()
             }
@@ -213,7 +201,7 @@ class OnlineChat {
         override fun run() {
             name = "AcceptThread"
             var socket: BluetoothSocket?
-            while (state !== STATE_CONNECTED) {
+            while (STATE_CONNECTED!= currentState ) {
                 try {
                     socket = serverSocket!!.accept()
                 } catch (e: IOException) {
@@ -223,7 +211,7 @@ class OnlineChat {
                 // If a connection was accepted
                 if (socket != null) {
                     synchronized(this@OnlineChat) {
-                        when (state) {
+                        when (currentState) {
                             STATE_LISTEN, STATE_CONNECTING ->
                                 // start the connected thread.
                                 connected(socket, socket.remoteDevice)
@@ -268,7 +256,7 @@ class OnlineChat {
             name = "ConnectThread"
 
             // Always cancel discovery because it will slow down a connection
-            bluetoothAdapter.cancelDiscovery()
+            bluetoothAdapter!!.cancelDiscovery()
 
             // Make a connection to the BluetoothSocket
             try {
@@ -330,9 +318,9 @@ class OnlineChat {
                     bytes = inputStream!!.read(buffer)
 
                     // Send the obtained bytes to the UI Activity
-                    handler.obtainMessage(
-                        MainActivity.MESSAGE_READ, bytes, -1,
-                        buffer
+                    handler!!.obtainMessage(
+                            MESSAGE_READ, bytes, -1,
+                            buffer
                     ).sendToTarget()
                 } catch (e: IOException) {
                     connectionLost()
@@ -348,9 +336,9 @@ class OnlineChat {
         fun write(buffer: ByteArray) {
             try {
                 outputStream!!.write(buffer)
-                handler.obtainMessage(
-                    MainActivity.MESSAGE_WRITE, -1, -1,
-                    buffer
+                handler!!.obtainMessage(
+                        MESSAGE_WRITE, -1, -1,
+                        buffer
                 ).sendToTarget()
             } catch (e: IOException) {
             }
